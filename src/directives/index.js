b8529299c40427ba024548ab4e5ae26e39b318d8
liuwei <liuweihjyalms@163.com>
2018-11-05 10:50:51 +0800

    dropdown logout sweetalert2

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,4 +1,16 @@
 import Vue from 'vue'
 import validator from './validator'
+import dropdown from './dropdown'
 
-Vue.directive('validator', validator)
+//注册指令
+//Vue.directive('validator', validator)
+
+const directives = {
+  validator,
+  dropdown
+}
+
+//使用一个循环，来注册我们的所有指令
+for (const [key, value] of Object.entries(directives)) {
+  Vue.directive(key, value)
+}
