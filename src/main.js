02fc29a87c33741e489b2be1a78e99bf2500bb5e
liuwei <liuweihjyalms@163.com>
2018-11-06 23:05:11 +0800

    articles of the article -moment - node-emoji

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import store from './store'
 import VueSweetalert2 from './plugins/vue-sweetalert2'
 // 引入插件
 import Message from './plugins/message'
+import './filters'
 
 // 使用插件
 Vue.use(VueSweetalert2)
