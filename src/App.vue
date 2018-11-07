19f3ead2655d8807feda09ed7a67eef2a44499d8
liuwei <liuweihjyalms@163.com>
2018-11-07 17:21:49 +0800

    animate.css like -zhang

--- src/App.vue
+++ src/App.vue
@@ -43,4 +43,6 @@ $fa-font-path: "~font-awesome/fonts/";
 @import "./styles/extra";
 @import 'simplemde/dist/simplemde.min.css';
 @import 'highlight.js/styles/paraiso-dark.css';
+// 引入 animate.css
+@import "animate.css/animate.min.css";
 </style>
