28c1fac21adb9d0f3cb87483de8eaab201768907
liuwei <liuweihjyalms@163.com>
2018-11-08 17:17:28 +0800

    add a list of posts 添加帖子列表

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ import VueSweetalert2 from './plugins/vue-sweetalert2'
 // 引入插件
 import Message from './plugins/message'
 import './filters'
+import { mockArticles } from './mock/data'
+import ls from './utils/localStorage'
 
 // 使用插件
 Vue.use(VueSweetalert2)
@@ -23,6 +25,28 @@ Vue.use(Message)
 
 // 设置 false 以阻止 Vue 在启动时生成生产提示
 Vue.config.productionTip = false
+
+const AddMockData = (() => {
+  // 是否加入测试数据
+  //如果不需要测试数据，可以设置 isAddMockData 为 false
+  const isAddMockData = true
+  // 用户数据
+  let userArticles = ls.getItem('articles')
+
+  if (Array.isArray(userArticles)) {
+    userArticles = userArticles.filter(article => parseInt(article.uid) === 1)
+  } else {
+    userArticles = []
+  }
+
+  if (isAddMockData) {
+    // 合并用户数据和测试数据，使用合并值作为所有文章
+    store.commit('UPDATE_ARTICLES', [...userArticles, ...mockArticles(10)])
+  } else {
+    // 使用用户数据作为所有文章
+    store.commit('UPDATE_ARTICLES', userArticles)
+  }
+})()
 
 // eslint 配置，允许 new 一个实例后不赋值，我们没有使用 eslint，如果有，则下一行注释不可缺少
 /* eslint-disable no-new */
