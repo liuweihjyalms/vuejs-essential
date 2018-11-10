0ead9930bd75275875dc747d2efbe41d657b1ac8
liuwei <liuweihjyalms@163.com>
2018-11-10 09:55:16 +0800

    添加搜索框

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -23,7 +23,10 @@
 
         <!-- 入口组件 -->
         <div class="navbar-right">
+          <!-- 搜索框 -->
+          <SearchInput/>
           <TheEntry/>
+
         </div>
 
       </div>
@@ -34,12 +37,16 @@
 <script>
 // 引入 TheEntry.vue 的默认值
 import TheEntry from '@/components/layouts/TheEntry'
+// 引入 SearchInput.vue 默认值
+import SearchInput from '@/components/layouts/SearchInput'
 
 export default {
   name: 'TheHeader',
   // 添加 components 选项，并注册 TheEntry
   components: {
-    TheEntry
+    TheEntry,
+    // 注册 SearchInput
+    SearchInput
   },
 
   data() {
--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,9 @@ const state = {
   // 添加 auth 来保存当前用户的登录状态
   auth: ls.getItem('auth'),
   // 所有文章状态
-  articles: ls.getItem('articles')
+  articles: ls.getItem('articles'),
+  // 搜索值
+  searchValue: ''
 }
 
 // 添加 getters
@@ -56,6 +58,10 @@ const mutations = {
   UPDATE_ARTICLES(state, articles) {
     state.articles = articles
     ls.setItem('articles', articles)
+  },
+  // 更新搜索值的事件类型
+  UPDATE_SEARCH_VALUE(state, searchValue) {
+    state.searchValue = searchValue
   }
 }
 
